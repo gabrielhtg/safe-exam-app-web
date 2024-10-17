6fd8dc285ce46c5d347247eaf701f5015af7e7e0
11S21048 <ifs21048@students.del.ac.id>
2024-10-17 14:08:31 +0700

    menambahkan field nama pada halaman register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,38 +1,77 @@
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
-      <>
-        <div className={'bg-cover bg-center h-screen w-full bg-[url(/assets/images/login-bg.jpg)]'}>
-          <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"></div>
-          <div className={'relative w-full h-screen flex justify-center items-center'}>
-            <Card className={'w-[380px] px-5'}>
-              <CardHeader>
-                <CardTitle className={'text-2xl text-center mb-3'}>Safe Exam Register</CardTitle>
-                <CardDescription className={'text-center'}>
-                  Created by TA-12 IF 21
-                </CardDescription>
-              </CardHeader>
+    <>
+      <div
+        className={
+          "bg-cover bg-center h-screen w-full bg-[url(/assets/images/login-bg.jpg)]"
+        }
+      >
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"></div>
+        <div
+          className={
+            "relative w-full h-screen flex justify-center items-center"
+          }
+        >
+          <Card className={"w-[380px] px-5"}>
+            <CardHeader>
+              <CardTitle className={"text-2xl text-center mb-3"}>
+                Safe Exam Register
+              </CardTitle>
+              <CardDescription className={"text-center"}>
+                Created by TA-12 IF 21
+              </CardDescription>
+            </CardHeader>
 
-              <CardContent>
-                <Input id={'input-username'} type={'text'} className={'mb-3'} placeholder={'Username'}></Input>
-                <Input id={'input-password'} type={'password'} placeholder={'Password'}></Input>
-              </CardContent>
+            <CardContent>
+              <Input
+                id={"input-name"}
+                type={"text"}
+                className={"mb-3"}
+                placeholder={"Name"}
+              ></Input>
+              <Input
+                id={"input-username"}
+                type={"text"}
+                className={"mb-3"}
+                placeholder={"Username"}
+              ></Input>
+              <Input
+                id={"input-password"}
+                type={"password"}
+                placeholder={"Password"}
+              ></Input>
+            </CardContent>
 
-              <CardFooter className={'gap-x-3 flex-col items-start'}>
-                <div className="flex gap-x-3 w-full">
-                  <Button id="button-register" className="w-full">Register</Button>
-                  <Button asChild={true} id="button-login" className="w-full" variant="secondary">
-                    <Link href={'/login'}>Login</Link>
-                  </Button>
-                </div>
-              </CardFooter>
-            </Card>
-          </div>
+            <CardFooter className={"gap-x-3 flex-col items-start"}>
+              <div className="flex gap-x-3 w-full">
+                <Button id="button-register" className="w-full">
+                  Register
+                </Button>
+                <Button
+                  asChild={true}
+                  id="button-login"
+                  className="w-full"
+                  variant="secondary"
+                >
+                  <Link href={"/login"}>Login</Link>
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
         </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
