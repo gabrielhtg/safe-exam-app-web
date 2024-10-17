c0ec0e7c3380b8299d31814cd1b097256878c3c5
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-17 10:40:38 +0700

    first commit

---
+++ app/password/reset/page.tsx
@@ -0,0 +1,39 @@
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {LockKeyhole} from "lucide-react";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+export default function ResetPasswordPage() {
+    return(
+        <div className={'flex items-center justify-center h-screen'}>
+            <Card className={'w-[400px]'}>
+                <CardHeader>
+                    <div className={'flex items-center justify-center mb-3'}>
+                        <div className={'border-2 border-black p-8 rounded-full'}>
+                            <LockKeyhole strokeWidth={1.25} className={'w-[30px] h-[30px]'}/>
+                        </div>
+                    </div>
+                    <CardTitle className={'text-center'}>
+                        Trouble Logging In?
+                    </CardTitle>
+
+                    <CardDescription className={'text-center'}>
+                        Enter your email or username and we'll send you a link to get back into your account.
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                    <Input id={'input-username-or-email'} placeholder={'Email, Username'}></Input>
+
+                    <Button className={'w-full mt-5'}>Send Login Link</Button>
+                    <Button className={'w-full mt-3'} variant={"secondary"} asChild={true}>
+                        <Link href={'/login'}>
+                            Back to Login
+                        </Link>
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
