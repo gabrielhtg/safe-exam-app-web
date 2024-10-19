4a31d21891d78094045e992ad726b64468130aed
11S21048 <ifs21048@students.del.ac.id>
2024-10-19 21:48:29 +0700

    page upload photo profile tapi belum bisa save photo

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,80 +5,83 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function RegisterPage() {
   return (
     <>
+      <div
+        className={'relative w-full h-screen flex justify-center items-center'}
+      >
+        <Card className={'w-[380px] px-5'}>
+          <CardHeader>
+            <CardTitle className={'text-2xl text-center mb-3'}>
+              Safe Exam Register
+            </CardTitle>
+            <CardDescription className={'text-center'}>
+              Created by TA-12 IF 21
+            </CardDescription>
+          </CardHeader>
 
-        <div
-          className={
-            "relative w-full h-screen flex justify-center items-center"
-          }
-        >
-          <Card className={"w-[380px] px-5"}>
-            <CardHeader>
-              <CardTitle className={"text-2xl text-center mb-3"}>
-                Safe Exam Register
-              </CardTitle>
-              <CardDescription className={"text-center"}>
-                Created by TA-12 IF 21
-              </CardDescription>
-            </CardHeader>
+          <CardContent>
+            <Input
+              id={'input-name'}
+              type={'text'}
+              className={'mb-3'}
+              placeholder={'Name'}
+            ></Input>
+            <Input
+              id={'input-username'}
+              type={'text'}
+              className={'mb-3'}
+              placeholder={'Username'}
+            ></Input>
+            <Input
+              id={'input-email'}
+              type={'text'}
+              className={'mb-3'}
+              placeholder={'Email'}
+            ></Input>
+            <Input
+              id={'input-password'}
+              type={'password'}
+              className={'mb-3'}
+              placeholder={'Password'}
+            ></Input>
+            <Input
+              id={'input-reenter-password'}
+              type={'password'}
+              className={'mb-3'}
+              placeholder={'Confirm Password'}
+            ></Input>
 
-            <CardContent>
-              <Input
-                id={"input-name"}
-                type={"text"}
-                className={"mb-3"}
-                placeholder={"Name"}
-              ></Input>
-              <Input
-                id={"input-username"}
-                type={"text"}
-                className={"mb-3"}
-                placeholder={"Username"}
-              ></Input>
-              <Input
-                id={"input-email"}
-                type={"text"}
-                className={"mb-3"}
-                placeholder={"Email"}
-              ></Input>
-              <Input
-                id={"input-password"}
-                type={"password"}
-                className={"mb-3"}
-                placeholder={"Password"}
-              ></Input>
-              <Input
-                id={"input-reenter-password"}
-                type={"password"}
-                className={"mb-3"}
-                placeholder={"Confirm Password"}
-              ></Input>
-            </CardContent>
+            <div className="flex justify-center w-full mt-3">
+              <Button asChild={true} id="button-upload-photo" variant="outline">
+                <Link href="/register/upload-photo">Upload Photo</Link>
+              </Button>
+            </div>
+          </CardContent>
 
-            <CardFooter className={"gap-x-3 flex-col items-start"}>
-              <div className="flex gap-x-3 w-full">
-                <Button id="button-register" className="w-full">
-                  Register
-                </Button>
-                <Button
-                  asChild={true}
-                  id="button-login"
-                  className="w-full"
-                  variant="secondary"
-                >
-                  <Link href={"/login"}>Login</Link>
-                </Button>
-              </div>
-            </CardFooter>
-          </Card>
-        </div>
+          <CardFooter className={'gap-x-3 flex-col items-start'}>
+            <div className="flex gap-x-3 w-full">
+              <Button id="button-register" className="w-full">
+                Register
+              </Button>
+              <Button
+                asChild={true}
+                id="button-login"
+                className="w-full"
+                variant="secondary"
+              >
+                <Link href={'/login'}>Login</Link>
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
     </>
-  );
+  )
 }
