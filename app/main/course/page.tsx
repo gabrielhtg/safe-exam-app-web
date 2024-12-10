4cca9382a5345b7441bb34db06fdb9562bf55d6d
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-12-10 22:26:37 +0700

    menambah fitur

--- app/main/course/page.tsx
+++ app/main/course/page.tsx
@@ -55,9 +55,11 @@ export default function CoursePage() {
   const [courseImage, setCourseImage] = useState('')
   const [courseImageFile, setCourseImageFile] = useState<File | undefined>()
   const [courseTitle, setCourseTitle] = useState('')
+  const [courseTitleOld, setCourseTitleOld] = useState('')
   const [courseDesc, setCourseDesc] = useState('')
   const currentUsername = useSelector(selectUser).username
   const [courseList, setCourseList] = useState([])
+  const [loadingTitle, setLoadingTitle] = useState('')
 
   const getAllCourse = async () => {
     const response = await axios.get(
@@ -72,8 +74,28 @@ export default function CoursePage() {
     getAllCourse().then()
   }, [])
 
+  const handleRemoveCourse = async (title: string) => {
+    try {
+      const removeResponse = await axios.delete(
+        `${apiUrl}/course/${title}`,
+        getBearerHeader(localStorage.getItem('token')!)
+      )
+
+      setErrDialog(true)
+      setDialogType(1)
+      setDialogMsg(removeResponse.data.message)
+      getAllCourse().then()
+    } catch (err: any) {
+      setErrDialog(true)
+      setDialogType(0)
+      setDialogMsg(err.response.data.message)
+      getAllCourse().then()
+    }
+  }
+
   const handleAddCourse = async () => {
     setIsLoadingCreate(true)
+    setLoadingTitle('Creating New Course')
     const formData = new FormData()
     formData.append('title', courseTitle)
     formData.append('description', courseDesc)
@@ -128,6 +150,46 @@ export default function CoursePage() {
     }
 
     return ''
+  }
+
+  const handleEditCourse = async () => {
+    setIsLoadingCreate(true)
+    setLoadingTitle('Updating Course')
+    const formData = new FormData()
+    formData.append('title', courseTitle)
+    formData.append('description', courseDesc)
+    formData.append('username', currentUsername)
+    formData.append('old_title', courseTitleOld)
+
+    try {
+      if (courseImageFile) {
+        const compressedImage = await imageCompression(
+          courseImageFile!,
+          compressOptions
+        )
+
+        formData.append('course_pict', compressedImage)
+      }
+
+      const response = await axios.patch(
+        `${apiUrl}/course`,
+        formData,
+        getBearerHeader(localStorage.getItem('token')!)
+      )
+
+      if (response.status === 200) {
+        getAllCourse().then()
+        setErrDialog(true)
+        setDialogType(1)
+        setDialogMsg(response.data.message)
+        setIsLoadingCreate(false)
+      }
+    } catch (err: any) {
+      setErrDialog(true)
+      setDialogType(0)
+      setDialogMsg(err.response.data.message)
+      setIsLoadingCreate(false)
+    }
   }
 
   return (
@@ -251,12 +313,126 @@ export default function CoursePage() {
                   <Button>
                     <Eye />
                   </Button>
-                  <Button>
-                    <Pencil />
-                  </Button>
-                  <Button variant={'destructive'}>
-                    <Trash2 />
-                  </Button>
+
+                  {/*Bagian Edit Dialog*/}
+                  <AlertDialog>
+                    <AlertDialogTrigger
+                      onClick={() => {
+                        setCourseTitle(course.title)
+                        setCourseDesc(course.description)
+                        setCourseImage(`${apiUrl}/${course.image}`)
+                        setCourseTitleOld(course.title)
+                      }}
+                    >
+                      <Button>
+                        <Pencil />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Edit Course</AlertDialogTitle>
+                        <AlertDialogDescription
+                          className={'flex flex-col gap-5 mt-3'}
+                        >
+                          <div className="grid w-full items-center gap-1.5 mt-3">
+                            <Label htmlFor="course-title-update">
+                              Course Title
+                            </Label>
+                            <Input
+                              type="text"
+                              id="course-title-update"
+                              value={courseTitle}
+                              onChange={(e) => {
+                                setCourseTitle(e.target.value)
+                              }}
+                            />
+                          </div>
+
+                          <div className="grid w-full items-center gap-1.5 ">
+                            <Label htmlFor="course-desc-update">
+                              Course Description
+                            </Label>
+                            <Textarea
+                              id="course-desc-update"
+                              value={courseDesc}
+                              onChange={(e) => {
+                                setCourseDesc(e.target.value)
+                              }}
+                            />
+                          </div>
+
+                          <div className="grid w-full items-center gap-1.5">
+                            <Label htmlFor="course-image">Course Image</Label>
+
+                            {course.image == null ? (
+                              <Skeleton
+                                className={
+                                  'w-full h-48 flex items-center justify-center'
+                                }
+                              >
+                                <ImagePlus />
+                              </Skeleton>
+                            ) : (
+                              <Image
+                                src={courseImage}
+                                width={500}
+                                height={500}
+                                alt={'course-image'}
+                                className={'w-full h-48 object-cover'}
+                              />
+                            )}
+
+                            <Input
+                              type="file"
+                              id="course-image"
+                              onChange={(e) => {
+                                setCourseImage(
+                                  URL.createObjectURL(e.target.files![0])
+                                )
+                                setCourseImageFile(e.target.files![0])
+                              }}
+                            />
+                          </div>
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={handleEditCourse}>
+                          Save
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+
+                  {/*Bagian Delete Dialog*/}
+                  <AlertDialog>
+                    <AlertDialogTrigger>
+                      <Button variant={'destructive'}>
+                        <Trash2 />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Delete Course</AlertDialogTitle>
+                        <AlertDialogDescription
+                          className={'flex flex-col gap-5 mt-3'}
+                        >
+                          <p>Are you sure to delete ${course.title} course?</p>
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <Button
+                          variant={'destructive'}
+                          onClick={() => {
+                            handleRemoveCourse(course.title)
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </CardFooter>
               </Card>
             ))}
@@ -294,7 +470,7 @@ export default function CoursePage() {
             <AlertDialogTitle
               className={'text-center flex flex-col items-center'}
             >
-              Creating New Course
+              {loadingTitle}
             </AlertDialogTitle>
             <AlertDialogDescription
               className={'flex w-full justify-center mt-3'}
