f41eeca8c027180765e4e65ef68237a9c58415fe
TiaraYu8 <yuliandari.tiara@gmail.com>
2025-01-01 17:39:16 +0700

    reverse apiUrl

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,4 @@
-export const apiUrl = 'http://localhost:3001'
-export const feUrl = 'http://localhost:3000'
-// export const apiUrl = 'http://172.27.68.74:3001'
-// export const feUrl = 'http://172.27.68.74:3000'
+export const apiUrl = 'https://thesis-be.getsuyoubi-monday.site'
 
 export const compressOptions = {
   maxSizeMB: 1,
