4b22905916000c3c89b8576542fb9f0984d10fd3
TiaraYu8 <yuliandari.tiara@gmail.com>
2025-01-03 18:21:56 +0700

    updating the apiUrl

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,5 @@
-export const apiUrl = 'https://thesis-be.getsuyoubi-monday.site'
+export const apiUrl = '10.147.18.79:4001'
+// export const apiUrl = 'https://thesis-be.getsuyoubi-monday.site'
 
 export const compressOptions = {
   maxSizeMB: 1,
