0c07959b510f20abdbf82dedcf65aa7b51ee6849
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2025-02-10 14:52:51 +0700

    fix bug

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,3 @@
-export const apiUrl = 'http://localhost:3001'
-export const feUrl = 'http://localhost:3000'
 // export const apiUrl = 'http://172.27.68.74:3001'
 // export const feUrl = 'http://172.27.68.74:3000'
 
