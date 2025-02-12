ccaa454da43fe7f1404e2d0c5cedabf5faeeea73
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2025-02-12 12:57:40 +0700

    menghapus sementara

--- app/main/exam/report/review-result/[id]/page.tsx
+++ app/main/exam/report/review-result/[id]/page.tsx
@@ -79,10 +79,7 @@ export default function ReviewResult({ params }: any) {
                 key={ansIndex}
                 className={'flex gap-3 rounded-lg p-5 pb-0 mr-5'}
               >
-                <div>
-                  {ansIndex + 1}.{' '}
-                  <span className={'font-bold'}>{answer.id}</span>
-                </div>
+                <div>{ansIndex + 1}.</div>
                 <div className={'w-full h-auto'}>
                   {parse(answer.question.content)}
                   {answer.question.type === 'essay' ? (
