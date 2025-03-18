086c4612a5ff6993f459572f3f1a792ca395ca3c
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2025-03-18 19:29:11 +0700

    membuat router.back()

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -174,8 +174,7 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
-        and <a href="#">Privacy Policy</a>.
+        Created By TA-12 Institut Teknologi Del
       </div>
 
       <AlertDialog open={errDialog}>
